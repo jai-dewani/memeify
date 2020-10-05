8129241609ff2f03e5cd3777a48abdc818c7e0d1
fletcherrippon <fletcherrippon@gmail.com>
2020-10-05 16:10:31 +1100

    Moved to components folder

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                // src="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png" 
+                src="http://assets.stickpng.com/images/580b585b2edbce24c47b2a2f.png"
+                alt="Problem?"
+                height="100px"
+                width="100px"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
