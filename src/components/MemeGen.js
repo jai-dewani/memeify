900665aae5161c592fa82ae44e2798411efab770
jai-dewani <jai.dewani.99@gmail.com>
2021-05-08 11:12:47 +0530

    Refacator usless code

--- src/components/MemeGen.js
+++ src/components/MemeGen.js
@@ -1,7 +1,7 @@
 import React, {Component } from "react"
 import DisplayMeme from './displayMeme'
-import $ from 'jquery';
-   
+import Canvas from './canvas'
+
 const checkImageHeight = (width, height, maxHeight) => {
     let newheight = height
     let newwidth = width
@@ -16,7 +16,30 @@ const checkImageHeight = (width, height, maxHeight) => {
 class MemeGenerator extends Component {
     constructor() {
         super()
+
         this.state = {
+            texts: [
+                {
+                    id: 0,
+                    text: "Top",
+                    color: "#000000",
+                    borderColor: "#FFFFFF",
+                    size: 10,
+                    x:50, 
+                    y:50
+                },
+                {
+                    id: 1,
+                    text: "Bottom",
+                    color: "#000000",
+                    borderColor: "#FFFFFF",
+                    size: 10,
+                    x:250, 
+                    y:250
+                },
+            ],
+            context: undefined, 
+            canvas: undefined,
             topText: "",
             bottomText: "",
             bottomColor: "#000000",
@@ -26,199 +49,200 @@ class MemeGenerator extends Component {
             url: "http://i.imgflip.com/1bij.jpg",
             allMemeImgs: [],
             image: undefined,
-            updateImage: false,
             maxHeight: Math.floor(window.innerHeight*0.8),
-            isDragging: false,
-            x: 50,
-            y: 50
-
+            updateImage: false,
+            // isDragging: false,
         }
         this.componentDidMount = this.componentDidMount.bind(this)
-        this.setImage = this.setImage.bind(this)
-        this.setText = this.setText.bind(this)
-        this.handleColorChange = this.handleColorChange.bind(this)
         this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleRandomClick = this.handleRandomClick.bind(this)
         this.handleFileChange = this.handleFileChange.bind(this)
+        this.handleAddText = this.handleAddText.bind(this)
+        this.handleRemoveText = this.handleRemoveText.bind(this)
+        this.selectedText = -1;
     }
+
+    
 
     componentDidMount() {
+        const { url } = this.state
+        var image = new Image()
+        image.src = url 
+        console.log(image.src)
         fetch("https://api.imgflip.com/get_memes")
-            .then(response => response.json())
-            .then(response => {
-                const { memes } = response.data
-                this.setState({ allMemeImgs: memes })
+        .then(response => response.json())
+        .then(response => {
+            const { memes } = response.data
+            this.setState({ allMemeImgs: memes, image })
+        }).then( () => {
+            var canvas = document.getElementById("my-canvas")
+            var context = canvas.getContext("2d");
+            this.setState({
+                context,
+                canvas,
             })
-        
-        this.setImage();
+            console.log(this.state);
+            // canvas.onmousedown = function(evt){
+            //     evt.preventDefault();
+            //     this.handleMouseDown(evt);
+            // }
+            // canvas.onmousemove = function(evt){
+            //     evt.preventDefault();
+            //     this.handleMouseMove(evt);
+            // }
+         
+            // canvas.onmouseup = function(evt){
+            //     evt.preventDefault();
+            //     this.handleMouseUp(evt);
+            // }
+          
+            // canvas.onmouseout = function(evt){
+            //     evt.preventDefault();
+            //     this.handleMouseOut(evt);
+            // }
+        })
     }
-   
-    setImage() {
-        const myCanvas = document.getElementById("my-canvas");
-        const context = myCanvas.getContext("2d");
-        const img = new Image()
-        const { url, width, height, maxHeight } = this.state
+    
+    componentDidUpdate() {
+        this.setImage()
+    }
+
+
+
+
+
+    // textHittest = (x, y, textIndex) => {
+    //     var text = this.texts[textIndex];
+    //     return (x >= text.x && x <= text.x + text.width && y >= text.y - text.height && y <= text.y);
+    // }
+
+    // handleMouseDown(e){
+    //     e.preventDefault();
+    //     var startX, startY;
+    //     const { offsetY, offsetX } = this.state; 
+    //     startX = parseInt(e.clientX - offsetX);
+    //     startY = parseInt(e.clientY - offsetY);
+    //     // Put your mousedown stuff here
+    //     for (var i = 0; i < this.texts.length; i++) {
+    //         if (this.textHittest(startX, startY, i)) {
+    //             this.selectedText = i;
+    //         }
+    //     }
+    // }
+
+    // handleMouseMove(e){
+    //     e.preventDefault()
+    //     if (this.selectedText < 0) {
+    //         return;
+    //     }
+    //     var canvasOffset = document.getElementById("my-canvas").offset();
+    //     var offsetX = canvasOffset.left;
+    //     var offsetY = canvasOffset.top;
+    //     var mouseX, mouseY, startX, startY
+    //     mouseX = parseInt(e.clientX - offsetX);
+    //     mouseY = parseInt(e.clientY - offsetY);
+    //     // Put your mousemove stuff here
+    //     var dx = mouseX - startX;
+    //     var dy = mouseY - startY;
+    //     startX = mouseX;
+    //     startY = mouseY;
+    
+    //     var text = this.state.texts[this.selectedText];
+    //     text.x += dx;
+    //     text.y += dy;
+    //     this.setImage()
+    // }
+
+    // handleMouseUp(e){
+    //     e.preventDefault();
+    //     this.selectedText = -1;
+    // }
+    // handleMouseOut = (e) => {
+    //     e.preventDefault();
+    //     this.selectedText = -1;
+    // }
+
+    setImage = () => {
+        console.log(this.state);
+        const { image, texts, height, width, url, maxHeight } = this.state
+        var canvas = document.getElementById("my-canvas")
+        var context = canvas.getContext("2d");
+        // context.clearRect(0, 0, canvas.width, canvas.height);
+        image.src = url;
         const { newheight, newwidth } = checkImageHeight(width, height, maxHeight);
-        img.src = url
-        img.onload = () => {
-            myCanvas.height = newheight
-            myCanvas.width = newwidth
-            context.drawImage(img, 0, 0, width, height, 0, 0, newwidth, newheight);
-        }
-        this.setState({ image: img })
-    }
-
-    setText() {
-        const myCanvas = document.getElementById("my-canvas");
-        const mySubmit1 = document.getElementById("submit1");
-        const context = myCanvas.getContext("2d");
-        const { image, topText, bottomText, bottomColor, bottomBorderColor, height, width } = this.state
-        context.drawImage(image, 0, 0, width, height, 0, 0, width, height);
-        var $canvas = $("#my-canvas");
-        var canvasOffset = $canvas.offset();
-        var offsetX = canvasOffset.left;
-        var offsetY = canvasOffset.top;
-        var startX;
-        var startY; 
-        var mouseX;
-        var mouseY;
-        var texts = [];   
-        var selectedText = -1;
-        // test if x,y is inside the bounding box of texts[textIndex]
-        function draw() {
-            context.clearRect(0, 0, myCanvas.width, myCanvas.height);
-            context.drawImage(image, 0, 0, width, height, 0, 0, width, height);
+        image.onload = () => {
+            canvas.height = newheight
+            canvas.width = newwidth
+            context.drawImage(image, 0, 0, width, height, 0, 0, newwidth, newheight);
             for (var i = 0; i < texts.length; i++) {
-                var text = texts[i];
-                context.fillText(text.text, text.x, text.y);
-            }
-           
-        }
-        function textHittest(x, y, textIndex) {
-            var text = texts[textIndex];
-            return (x >= text.x && x <= text.x + text.width && y >= text.y - text.height && y <= text.y);
-        }
-
-
-        function handleMouseDown(e) {
-            e.preventDefault();
-            startX = parseInt(e.clientX - offsetX);
-            startY = parseInt(e.clientY - offsetY);
-            // Put your mousedown stuff here
-            for (var i = 0; i < texts.length; i++) {
-                if (textHittest(startX, startY, i)) {
-                    selectedText = i;
-                }
-            }
-        }
-        function handleMouseUp(e) {
-            e.preventDefault();
-            selectedText = -1;
-        }
-        function handleMouseOut(e) {
-            e.preventDefault();
-            selectedText = -1;
-        }
-        function handleMouseMove(e) {
-            if (selectedText < 0) {
-                return;
-            }
-            e.preventDefault();
-            mouseX = parseInt(e.clientX - offsetX);
-            mouseY = parseInt(e.clientY - offsetY);
-            // Put your mousemove stuff here
-            var dx = mouseX - startX;
-            var dy = mouseY - startY;
-            startX = mouseX;
-            startY = mouseY;
-
-            var text = texts[selectedText];
-            text.x += dx;
-            text.y += dy;
-            draw()
-            
-        } 
-
-        myCanvas.onmousedown = function(evt){
-            evt.preventDefault()
-            handleMouseDown(evt);
-        }
-        myCanvas.onmousemove = function(evt){
-            evt.preventDefault()
-            handleMouseMove(evt);
-        }
-     
-        myCanvas.onmouseup = function(evt){
-            evt.preventDefault()
-            handleMouseUp(evt);
-        }
-      
-        myCanvas.onmouseout = function(evt){
-            evt.preventDefault()
-            handleMouseOut(evt);
-        }
-        mySubmit1.onclick = function (evt) {
-            evt.preventDefault()
-
-            if(texts.length === 0) {
-                // calc the y coordinate for this text on the canvas
-                var y = texts.length * 20 + 20;
-            
-                // get the text from the input element
-                var text1 = {
-                    text: topText,
-                    x: 20,
-                    y: y,
-                    flag:false
-                };
-                var text2 = {
-                    text: bottomText,
-                    x: 20,
-                    y: y,
-                    flag:true
-                }
-
-            
-                // calc the size of this text for hit-testing purposes
                 context.font = "2em impact, sans-serif"
-                text1.width = context.measureText(text1.text).width;
-                text2.width = context.measureText(text2.text).width;
-                context.strokeStyle = bottomBorderColor
-                context.strokeText(text1.text, width / 2 - text1.width / 2, 30)
-                context.strokeText(text2.text, width / 2 - text2.width / 2, 30)
-                context.fillStyle = bottomColor
-                context.fillText(text1.text, width / 2 - text1.width / 2, 30)
-                context.fillText(text2.text, width / 2 - text2.width / 2, 30)
-                text1.height = 36;
-                text2.height = 36;
-                // put this new text in the texts array
-                texts.push(text1);
-                texts.push(text2);
-                // redraw everything
-                draw();
+                const { text, color, borderColor, x, y, size } = texts[i];
+                const topWidth = context.measureText(text).width
+                context.strokeStyle = borderColor
+                context.strokeText(text, x, y)
+                context.fillStyle = color
+                context.fillText(text, x, y)
             }
         }
-        
     }
-
-    handleColorChange(event) {
+    
+    handleColorChange = (i, event) => {
         const { name, value } = event.target
+
         setTimeout(() => {
+            var texts = [...this.state.texts];
+            texts[i].color = value; 
+
             this.setState({ [name]: value })
         }, 250);
     }
 
-    handleChange(event) {
-        event.preventDefault()
+    handleBorderColorChange = (i, event) => {
         const { name, value } = event.target
-        this.setState({ [name]: value})
+
+        setTimeout(() => {
+            var texts = [...this.state.texts];
+            texts[i].borderColor = value; 
+
+            this.setState({ [name]: value })
+        }, 250);
     }
 
-    handleSubmit(event) {
+    handleChange(i, event) {
+        event.preventDefault()
+        const { value } = event.target
+        var texts = [...this.state.texts];
+        texts[i].text = value;
+        this.setState({ texts })
+    }
+
+    handleAddText(event){
+        event.preventDefault()
+        var texts = [...this.state.texts]
+        texts.push({
+            id:texts.length,
+            text: "",
+            color: "#000000",
+            borderColor: "#FFFFFF",
+            size: 10,
+            x:50, 
+            y:50
+        })
+        this.setState({ texts });
+    }
+
+    handleRemoveText(event){
+        event.preventDefault()
+        var texts = this.state.texts.slice(0,-1);
+        this.setState({ texts });
+    }
+
+    handleRandomClick(event) {
         event.preventDefault()
         const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
         const { url, width, height } = this.state.allMemeImgs[randNum]
         this.setState({ url: url, width: width, height: height, updateImage: true})
+        console.log(this.state)
     }
 
     handleFileChange(event) {
@@ -232,26 +256,24 @@ class MemeGenerator extends Component {
         img.src = url;
     }
 
-    componentDidUpdate() {
-        if (this.state.updateImage) {
-            this.setState({ updateImage: false })
-            this.setImage()
-        }
 
-        this.setText();
-      
-    }
 
     render() {
         return (
-            <DisplayMeme 
-                state={ this.state } 
-                handleSubmit={ this.handleSubmit } 
-                handleChange={ this.handleChange }
-                handleChangeBT={ this.handleChangeBT }
-                handleColorChange={ this.handleColorChange }
-                handleFileChange={ this.handleFileChange } 
-            />
+            <div className="memeRoot">
+                <DisplayMeme 
+                    state={ this.state } 
+                    handleRandomClick={ this.handleRandomClick } 
+                    handleChange={ this.handleChange }
+                    handleChangeBT={ this.handleChangeBT }
+                    handleColorChange={ this.handleColorChange }
+                    handleBorderColorChange={ this.handleBorderColorChange }
+                    handleFileChange={ this.handleFileChange } 
+                    handleAddText={ this.handleAddText }
+                    handleRemoveText={ this.handleRemoveText }
+                />
+                <Canvas />
+            </div>
         )
     }
 }
