53ec7855deeee6d424d4e0c765c45469cc221177
Jai <jai.dewani.99@gmail.com>
2021-11-15 01:06:08 +0530

    Finally rebased canvas 🚀

--- src/Components/canvas.js
+++ src/Components/canvas.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import DragableText  from "./DragableText";
+import { Stage, Layer } from "react-konva";
+// import checkImageHeight from "../Utils/checkImageHeight";
 
+// props contains => imageData, texts, handleExport(layer), setImage(layer, image)
+const Canvas = (props) => {
+	// const [image, setImage] = React.useState(props.image);
+	const [texts, setTexts] = useState(props.texts)
+	const [selectedId, selectText] = useState(null);
+	const layerRef = useRef(null);
 
-function canvas(props){
-    return (
-        <div className="meme">
-            <center>
-                <canvas 
-                    id="my-canvas" 
-                    className="inverted" 
-                    width="568px" height="335px"
-                    />
-            </center>
-        </div>
-    )
+	const checkDoSelect = (e) => {
+		const clickedOnEmpty = e.target === e.target.getStage();
+		if (clickedOnEmpty) {
+			selectText(null);
+		}
+	};
+
+	useEffect(() => {
+		props.setImage(layerRef.current,props.imageData);
+	})
+
+	return (
+		<div className="canvas inverted" >
+			<Stage 
+				width={props.imageData.width}
+				height={props.imageData.height}
+				onContentMouseover
+				onMouseDown={checkDoSelect}
+				onTouchStart={checkDoSelect}
+			>
+				<Layer ref={layerRef}>
+					
+				</Layer>
+				<Layer>
+					{props.texts.map((text, i) => {
+						return (
+							<DragableText
+								key={i}
+								shapeProps={text}
+								isSelected={text.id === selectedId}
+								onSelect={ () => selectText(text.id) }
+								onChange={(newAttrs) => {
+									const rects = props.texts.slice();
+									rects[i] = newAttrs;
+									props.setText(rects);
+								}}
+							/>
+						);
+					})}
+				</Layer>
+			</Stage>
+		</div>
+	);
 }
 
-export default canvas
+export default Canvas
